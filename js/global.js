22587239a4e46bd878b13f22251939c19d692f8e
desn2023 <seanreibling@outlook.com>
2023-11-26 20:16:52 -0500

    Full site

--- js/global.js
+++ js/global.js
@@ -1,5 +1,12 @@
 const swup = new Swup();
 
 swup.on('pageView', () => {
-    // This runs after every page change
-  });
+  // This runs after every page change
+
+  function scrollTop() {
+    setTimeout(function () {
+      window.scrollTo(0, 0)}, 50);
+  }
+
+  scrollTop();
+});
