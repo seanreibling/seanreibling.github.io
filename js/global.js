9126f0d9109cc3360604111fedf80badb053f7a4
desn2023 <seanreibling@outlook.com>
2023-12-06 18:08:48 -0500

    nav nenu

--- js/global.js
+++ js/global.js
@@ -5,8 +5,26 @@ swup.on('pageView', () => {
 
   function scrollTop() {
     setTimeout(function () {
-      window.scrollTo(0, 0)}, 50);
+      window.scrollTo(0, 0)
+    }, 50);
   }
 
   scrollTop();
 });
+
+
+let prevScrollPos = window.pageYOffset;
+const navbar = document.getElementById("nav");
+const navbarHeight = navbar.offsetHeight;
+
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  if ((prevScrollPos > currentScrollPos) || (currentScrollPos <= 24)) {
+    navbar.classList.remove('slide--up');
+    navbar.classList.add('slide--down');
+  } else {
+    navbar.classList.add('slide--up');
+    navbar.classList.remove('slide--down');
+  }
+  prevScrollPos = currentScrollPos;
+};
