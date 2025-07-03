4cd3d7767f00b646d054d15bb563aa8767e458e8
seanreibling <29679089+seanreibling@users.noreply.github.com>
2025-07-03 15:27:03 -0400

    V2.0

--- js/global.js
+++ js/global.js
@@ -10,8 +10,8 @@ swup.hooks.on('page:view', () => {
   checkHomeContactLink();
   projectCard();
   projectCardLocked();
-  urlCheck();
-  urlHover();
+  // urlCheck();
+  // urlHover();
   createPlayPauseButtons();
   aboutTextAnimate();    
 });
@@ -22,8 +22,6 @@ swup.hooks.on('visit:end', () => {
   initializeSlideshowsInContent();
   resizeImagesInSlideshows();
 });
-
-
 
 
 
@@ -39,49 +37,171 @@ function scrollTop() {
 
 
 
+
+//Show and hide nav menu when scrolling up and down
+
+let prevScrollPos = window.pageYOffset;
+const navbar = document.getElementById("nav");
+const stickyCol = document.querySelector(".masonry-col.is--sticky");
+
+const defaultPadding = 92;
+const reducedPadding = 36;
+
+window.onscroll = function scrollUpDown() {
+  const subnav = document.getElementById("subnav");
+  const currentScrollPos = window.pageYOffset;
+
+  const scrollingUp = prevScrollPos > currentScrollPos || currentScrollPos <= 24;
+
+  if (scrollingUp) {
+    // Show navbar
+    navbar.classList.remove("slide--up");
+    navbar.classList.add("slide--down");
+
+    // Adjust sticky padding
+    if (stickyCol) {
+      stickyCol.style.paddingTop = `${defaultPadding}px`;
+    }
+
+    // Show subnav (if exists)
+    if (subnav) {
+      subnav.classList.add("slide--up");
+      subnav.classList.remove("slide--down");
+    }
+  } else {
+    // Hide navbar
+    navbar.classList.add("slide--up");
+    navbar.classList.remove("slide--down");
+
+    // Adjust sticky padding
+    if (stickyCol) {
+      stickyCol.style.paddingTop = `${reducedPadding}px`;
+    }
+
+    // Hide subnav (if exists)
+    if (subnav) {
+      setTimeout(() => {
+        subnav.classList.remove("slide--up");
+        subnav.classList.add("slide--down");
+      }, 100);
+    }
+  }
+
+  prevScrollPos = currentScrollPos;
+};
+
+
+
+//Project Cover Hover In/Out
+
+function initGridItemHover() {
+  document.querySelectorAll('.grid-item').forEach(item => {
+    const img = item.querySelector('img');
+    if (!img) return;
+
+    item.addEventListener('mouseenter', () => {
+      img.style.opacity = '0.85';
+    });
+
+    item.addEventListener('mouseleave', () => {
+      img.style.opacity = '1';
+    });
+  });
+}
+
+// Initial run
+initGridItemHover();
+
+// Re-run after Swup content is replaced
+if (window.swup) {
+  swup.hooks.on('content:replace', () => {
+    initGridItemHover();
+  });
+}
+
+
+
+
+
+//Button Hover In/Out
+
+function initBtnIconHover() {
+  const buttons = document.querySelectorAll('.btn');
+
+  buttons.forEach(btn => {
+    const icon = btn.querySelector('.btn__icon');
+    if (!icon) return;
+
+    // Ensure transition is applied once
+    icon.style.transition = 'transform 0.3s ease';
+
+    btn.addEventListener('mouseenter', () => {
+      icon.style.transform = 'translateX(2px)';
+    });
+
+    btn.addEventListener('mouseleave', () => {
+      icon.style.transform = 'translateX(0)';
+    });
+  });
+}
+
+// Run once on initial page load
+initBtnIconHover();
+
+// Re-run after Swup replaces content
+if (window.swup) {
+  swup.hooks.on('content:replace', () => {
+    initBtnIconHover();
+  });
+}
+
+
+
+
+
 //Check URL and highlight links
 
-const linkContainer = document.getElementById('links');
-let links = linkContainer.querySelectorAll('a');
+// const linkContainer = document.getElementById('links');
+// let links = linkContainer.querySelectorAll('a');
 
-function urlCheck() {
-  links.forEach(link => {
-    if ((link.href.includes('portfolio')) && (window.location.href.includes('portfolio'))) {
-      link.style.color = 'black';
-      link.setAttribute('data-status', 'active');
-    } else if ((link.href.includes('about')) && (window.location.href.includes('about'))) {
-      link.style.color = 'black'
-      link.setAttribute('data-status', 'active');
-    } else {
-      link.style.color = 'rgba(0, 0, 0, 0.4)';
-      link.removeAttribute('data-status');
-    }
-  });
-}
+// function urlCheck() {
+//   links.forEach(link => {
+//     if ((link.href.includes('portfolio')) && (window.location.href.includes('portfolio'))) {
+//       link.style.color = 'white';
+//       link.setAttribute('data-status', 'active');
+//     } else if ((link.href.includes('about')) && (window.location.href.includes('about'))) {
+//       link.style.color = 'white'
+//       link.setAttribute('data-status', 'active');
+//     } else {
+//       link.style.color = 'white';
+//       link.removeAttribute('data-status');
+//     }
+//   });
+// }
 
-urlCheck();
+// urlCheck();
 
-function urlHover() {
-  links.forEach(link => {
-    if (!link.hasAttribute(`data-status`)) {
-      link.addEventListener('mouseover', function () {
-        link.style.color = 'black';
-      })
-    }
+// function urlHover() {
+//   links.forEach(link => {
+//     if (!link.hasAttribute(`data-status`)) {
+//       link.addEventListener('mouseover', function () {
+//         link.style.color = 'white';
+//       })
+//     }
 
-    if (!link.hasAttribute(`data-status`)) {
-      link.addEventListener('mouseout', function () {
-        link.style.color = 'rgba(0, 0, 0, 0.4)';
-      });
-    } else {
-      link.addEventListener('mouseout', function () {
-        link.style.color = 'black';
-      });
-    }
-  });
-}
+//     if (!link.hasAttribute(`data-status`)) {
+//       link.addEventListener('mouseout', function () {
+//         link.style.color = 'white';
+//       });
+//     } else {
+//       link.addEventListener('mouseout', function () {
+//         link.style.color = 'white';
+//       });
+//     }
+//   });
+// }
 
-urlHover();
+// urlHover();
 
 
 
@@ -100,399 +220,6 @@ function scrollEnable() {
 }
 
 
-
-
-
-//Open and close mobile nav menu
-
-const mobileNav = document.getElementById('nav__mobile');
-const mobileMenuBtn = document.getElementById('nav__btn');
-const mobileCloseBtn = document.getElementById('close__btn');
-
-
-
-mobileNav.classList.add('nav__collapse');
-document.getElementById('mobile__link1').classList.add('link__hide');
-document.getElementById('mobile__link2').classList.add('link__hide');
-// document.getElementById('mobile__link3').classList.add('link__hide');
-document.getElementById('mobile__link4').classList.add('link__hide');
-document.getElementById('mobile__link5').classList.add('link__hide');
-document.getElementById('mobile__link6').classList.add('link__hide');
-document.getElementById('mobile__link7').classList.add('link__hide');
-document.getElementById('mobile__link8').classList.add('link__hide');
-
-
-function mobileNavOpen() {
-
-  scrollDisable();
-  mobileNav.classList.remove('is--hidden');
-  mobileMenuBtn.classList.add('is--hidden');
-  mobileCloseBtn.classList.remove('is--hidden');
-
-  setTimeout(function () {
-    mobileNav.classList.remove('nav__collapse');
-    mobileNav.classList.add('nav__expand');
-  }, 25);
-
-  setTimeout(function () {
-    document.getElementById('mobile__link1').classList.remove('link__hide');
-    document.getElementById('mobile__link1').classList.add('link__show');
-  }, 150);
-  setTimeout(function () {
-    document.getElementById('mobile__link2').classList.remove('link__hide');
-    document.getElementById('mobile__link2').classList.add('link__show');
-  }, 200);
-  // setTimeout(function() {
-  //   document.getElementById('mobile__link3').classList.remove('link__hide');
-  //   document.getElementById('mobile__link3').classList.add('link__show');
-  // }, 0);
-  setTimeout(function () {
-    document.getElementById('mobile__link4').classList.remove('link__hide');
-    document.getElementById('mobile__link4').classList.add('link__show');
-  }, 250);
-  setTimeout(function () {
-    document.getElementById('mobile__link5').classList.remove('link__hide');
-    document.getElementById('mobile__link5').classList.add('link__show');
-  }, 400);
-  setTimeout(function () {
-    document.getElementById('mobile__link6').classList.remove('link__hide');
-    document.getElementById('mobile__link6').classList.add('link__show');
-  }, 450);
-  setTimeout(function () {
-    document.getElementById('mobile__link7').classList.remove('link__hide');
-    document.getElementById('mobile__link7').classList.add('link__show');
-  }, 500);
-  setTimeout(function () {
-    document.getElementById('mobile__link8').classList.remove('link__hide');
-    document.getElementById('mobile__link8').classList.add('link__show');
-  }, 550);
-}
-
-function mobileNavClose() {
-
-  scrollEnable();
-
-  mobileMenuBtn.classList.remove('is--hidden');
-  mobileCloseBtn.classList.add('is--hidden');
-
-  setTimeout(function () {
-    document.getElementById('mobile__link8').classList.add('link__hide');
-    document.getElementById('mobile__link8').classList.remove('link__show');
-  }, 25);
-  setTimeout(function () {
-    document.getElementById('mobile__link7').classList.add('link__hide');
-    document.getElementById('mobile__link7').classList.remove('link__show');
-  }, 50);
-  setTimeout(function () {
-    document.getElementById('mobile__link6').classList.add('link__hide');
-    document.getElementById('mobile__link6').classList.remove('link__show');
-  }, 75);
-  setTimeout(function () {
-    document.getElementById('mobile__link5').classList.add('link__hide');
-    document.getElementById('mobile__link5').classList.remove('link__show');
-  }, 100);
-  setTimeout(function () {
-    document.getElementById('mobile__link4').classList.add('link__hide');
-    document.getElementById('mobile__link4').classList.remove('link__show');
-  }, 200);
-  // setTimeout(function() {
-  //   document.getElementById('mobile__link3').classList.add('link__hide');
-  //   document.getElementById('mobile__link3').classList.remove('link__show');
-  // }, 0);
-  setTimeout(function () {
-    document.getElementById('mobile__link2').classList.add('link__hide');
-    document.getElementById('mobile__link2').classList.remove('link__show');
-  }, 250);
-  setTimeout(function () {
-    document.getElementById('mobile__link1').classList.add('link__hide');
-    document.getElementById('mobile__link1').classList.remove('link__show');
-  }, 300);
-
-  setTimeout(function () {
-    mobileNav.classList.add('nav__collapse');
-    mobileNav.classList.remove('nav__expand');
-  }, 100);
-
-  setTimeout(function () {
-    mobileNav.classList.add('is--hidden');
-  }, 600);
-}
-
-mobileMenuBtn.addEventListener('click', function () { mobileNavOpen(); });
-mobileCloseBtn.addEventListener('click', function () { mobileNavClose(); });
-
-//Simulate click on shadow link to delay navigation until menu is closed
-document.getElementById('mobile__link1').addEventListener('click', function () {
-  mobileNavClose();
-  setTimeout(function () {
-    document.getElementById('shadow__link1').click();
-  }, 400);
-});
-
-document.getElementById('mobile__link2').addEventListener('click', function () {
-  mobileNavClose();
-  setTimeout(function () {
-    document.getElementById('shadow__link2').click();
-  }, 400);
-});
-
-
-
-
-
-
-//Simulate click on logo and close mobile nav if needed
-document.getElementById('wordmark').addEventListener('click', function () {
-  if (mobileNav.classList.contains('is--hidden')) {
-    document.getElementById('shadow__link0').click();
-  } else {
-    mobileNavClose();
-    setTimeout(function () {
-      document.getElementById('shadow__link0').click();
-    }, 400);
-  }
-});
-
-//simulate click on contact link
-document.getElementById('mobile__link4').addEventListener('click', function () {
-  mobileNavClose();
-  showContact();
-});
-
-
-
-
-//Close nav menu if device width is greater than 479px
-
-function checkWindowWidth() {
-  const windowWidth = window.innerWidth;
-
-  if (windowWidth > 479) {
-    mobileNavClose();
-  }
-}
-
-window.addEventListener('resize', checkWindowWidth);
-
-
-
-//Show and hide nav menu when scrolling up and down
-
-let prevScrollPos = window.pageYOffset;
-const navbar = document.getElementById("nav");
-
-window.onscroll = function scrollUpDown() {
-  const subnav = document.getElementById("subnav");
-  const currentScrollPos = window.pageYOffset;
-  if ((prevScrollPos > currentScrollPos) || (currentScrollPos <= 24)) {
-    navbar.classList.remove('slide--up');
-    navbar.classList.add('slide--down');
-    if (subnav) {
-      subnav.classList.add('slide--up');
-      subnav.classList.remove('slide--down');
-    } else { }
-  } else {
-    navbar.classList.add('slide--up');
-    navbar.classList.remove('slide--down');
-    if (subnav) {
-      setTimeout(function () {
-        subnav.classList.remove('slide--up');
-        subnav.classList.add('slide--down');
-      }, 100)
-    } else { }
-  }
-  prevScrollPos = currentScrollPos;
-};
-
-
-
-
-
-
-
-//Show and hide contact card
-
-const form = document.getElementById('form');
-const formInputs = form.querySelectorAll('input, textarea, select');
-
-const contact = document.getElementById("contact");
-const contactCard = document.getElementById("contact__card");
-const submitCard = document.getElementById("contact__submit");
-const triggerOpen = document.getElementById("trigger--open");
-const triggerClose = document.getElementById("trigger--close");
-const submitClose = document.getElementById("submit--close");
-const triggerCloseBg = document.getElementById("contact__bg");
-
-
-function showContact() {
-
-  scrollDisable();
-  contact.classList.remove('is--hidden');
-  setTimeout(function () {
-    contactCard.scrollTop = 0;
-    triggerCloseBg.classList.remove('is--transparent');
-    contactCard.classList.add('slide--left');
-    contactCard.classList.remove('slide--right');
-  }, 50)
-}
-
-function hideContact() {
-  scrollEnable();
-  triggerCloseBg.classList.add('is--transparent');
-  contactCard.classList.remove('slide--left');
-  contactCard.classList.add('slide--right');
-  submitCard.classList.add('slide--right');
-  setTimeout(function () {
-    contact.classList.add("is--hidden");
-    submitCard.classList.remove('slide--right');
-  }, 350);
-
-  // Clear all form fields
-  formInputs.forEach(input => {
-    if (input.tagName === 'INPUT' || input.tagName === 'TEXTAREA') {
-      input.value = '';
-    } else if (input.tagName === 'SELECT') {
-      input.selectedIndex = 0;
-    }
-  });
-
-  //if in submitted state, revert to contact card
-  document.getElementById('contact__submit').classList.add('is--hidden');
-  document.getElementById('contact__card').classList.remove('is--hidden');
-  document.getElementById('timer__bar').classList.remove('is--filled');
-  clearTimeout(closeSubmitTimeout);
-}
-
-
-
-
-function hideContactCheck() {
-  function checkFormChanges() {
-    let formChanged = false;
-
-    formInputs.forEach(input => {
-      if (
-        (input.tagName === 'INPUT' || input.tagName === 'TEXTAREA') && input.defaultValue !== input.value ||
-        (input.tagName === 'SELECT' && input.selectedIndex !== 0)
-      ) {
-        formChanged = true;
-      }
-    });
-
-    if (formChanged) {
-      const confirmClose = window.confirm("Are you sure you want to close this form?");
-      if (confirmClose) {
-        hideContact(); // Call hideContact if changes detected and confirmed
-      }
-    } else {
-      hideContact(); // Call hideContact directly if no changes detected
-    }
-  }
-
-  // Call checkFormChanges when needed, e.g., when closing or navigating away from the page
-  // Example: checkFormChanges();
-
-  // Simulate calling checkFormChanges when a user attempts to close the form
-  // Replace this with your actual event trigger or call the function as needed
-  // For instance, attach it to a 'Close' button or a 'beforeunload' event
-  checkFormChanges();
-}
-
-triggerOpen.addEventListener('click', function () { showContact(); });
-triggerClose.addEventListener('click', function () { hideContactCheck(); });
-submitClose.addEventListener('click', function () { hideContact(); });
-triggerCloseBg.addEventListener('click', function () { hideContactCheck(); });
-
-//check if this page is homepage
-function checkHomeContactLink() {
-
-  const triggerOpenHome = document.getElementById("trigger--home");
-
-  if (triggerOpenHome) {
-    triggerOpenHome.addEventListener('click', function () { showContact(); });
-  } else { }
-}
-checkHomeContactLink();
-
-
-
-
-
-// Textarea form input customization
-
-const textarea = document.getElementById('form__message');
-const defaultLines = 6; // Change this to your desired default number of lines
-
-function updateTextarea() {
-  textarea.style.height = 'auto';
-  textarea.style.height = textarea.scrollHeight + 'px';
-}
-
-textarea.addEventListener('input', function () {
-  const lineBreaks = (this.value.match(/\n/g) || []).length;
-  const totalLines = lineBreaks + 1;
-
-  if (totalLines > defaultLines) {
-    if (this.value.slice(-1) !== '\n') {
-      this.value += '\n';
-      updateTextarea();
-    }
-  } else {
-    updateTextarea();
-  }
-});
-
-textarea.addEventListener('keydown', function (e) {
-  if (e.key === 'Backspace' && textarea.scrollHeight > textarea.offsetHeight) {
-    setTimeout(updateTextarea, 0);
-  }
-});
-
-updateTextarea();
-
-
-
-
-//Stop form refresh when submitting
-
-document.getElementById('form').addEventListener('submit', function (event) {
-  event.preventDefault(); // Prevent the default form submission
-
-  // Get form data
-  const formData = new FormData(this);
-
-  // Send form data to Formspree using Fetch API
-  fetch('https://formspree.io/f/xpzvabzd', {
-    method: 'POST',
-    body: formData,
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      // Handle successful form submission response
-      console.log('Form submitted successfully', data);
-
-      // Hide contact__card, show contact__submit for 5 seconds, then revert
-
-      document.getElementById('contact__card').classList.add('is--hidden');
-      document.getElementById('contact__submit').classList.remove('is--hidden');
-      submitCard.scrollTop = 0;
-      setTimeout(() => {
-        document.getElementById('timer__bar').classList.add('is--filled');
-      }, 50);
-
-
-      let closeSubmitTimeout = setTimeout(() => {
-        hideContact();
-      }, 8000);
-    })
-    .catch(error => {
-      // Handle errors
-      console.error('Error submitting form', error);
-    });
-});
 
 
 
@@ -544,11 +271,11 @@ function projectCard() {
     const text = project.querySelector('.heading.is--dim');
 
     project.addEventListener('mouseover', function () {
-      text.style.color = 'black';
+      text.style.color = 'white';
     });
 
     project.addEventListener('mouseout', function () {
-      text.style.color = 'rgba(0, 0, 0, 0.4)';
+      text.style.color = 'rgba(255, 255, 0, 0.4)';
     });
   });
 }
